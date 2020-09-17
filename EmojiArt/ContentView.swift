63041530cd8e4237c1dd4f6bd741e8edd9819a5f
Leozitor Floro de Souza <leozitor@gmail.com>
2020-09-17 15:18:20 -0400

    Initial Commit

---
+++ EmojiArt/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  EmojiArt
+//
+//  Created by Leozítor Floro on 17/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Leozítor Floro on 17/09/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
